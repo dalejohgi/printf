99b61d1bb0bf9bfd997499160ee75ae725b35570
mauricioolarte <reneolarte@yahoo.com>
2020-03-17 23:04:11 +0000

    add counter in int.c

--- int.c
+++ int.c
@@ -10,17 +10,18 @@
  */
 int print_number(va_list selector)
 {
-	int i, d, k = 1, p = 1, j, larg, flag, n;
+	int i, d, k = 1, p = 1, j, larg, flag = 0, neg = 0, n, len;
 
 	n = (va_arg(selector, int));
 	if (n == -2147483648)
 	{
 		n = n / 10;
-		flag = 2;
+		flag = 1;
 	}
 	if (n < 0)
 	{
 		n = -n;
+		neg = 1;
 		putchar('-');
 	}
 	larg = n;
@@ -29,44 +30,46 @@ int print_number(va_list selector)
 		larg = larg / 10;
 		k++;
 	}
+	len = k;
 	for (i = k; i > 0; i--)
 	{
 		if (k != 1)
 		{
-		for (j = 2; j <= i; j++)
-		{
-			p = p * 10;
-		}
+			for (j = 2; j <= i; j++)
+			{
+				p = p * 10;
+			}
 		}
 		d = n / p;
 		putchar(d + '0');
 		n = n - p * d;
 		p = 1;
 	}
-	if (flag == 2)
-	{
+	if (flag == 1)
 		putchar('8');
-	}
-	return (0);
+	if (neg == 1)
+		return (len + neg + flag);
+	return (len);
 }
 /**
  *print_numd - print a number whit putchar.
  *@selector: is a integer.
  *Return: 0
-*/
+ */
 int print_numd(va_list selector)
 {
-	int i, d, k = 1, p = 1, j, larg, flag, n;
+	int i, d, k = 1, p = 1, j, larg, flag = 0, n, len, neg = 0;
 
 	n = (va_arg(selector, int));
 	if (n == -2147483648)
 	{
 		n = n / 10;
-		flag = 2;
+		flag = 1;
 	}
 	if (n < 0)
 	{
 		n = -n;
+		neg = 1;
 		putchar('-');
 	}
 	larg = n;
@@ -75,25 +78,26 @@ int print_numd(va_list selector)
 		larg = larg / 10;
 		k++;
 	}
+	len = k;
 	for (i = k; i > 0; i--)
 	{
 		if (k != 1)
 		{
-		for (j = 2; j <= i; j++)
-		{
-			p = p * 10;
-		}
+			for (j = 2; j <= i; j++)
+			{
+				p = p * 10;
+			}
 		}
 		d = n / p;
 		putchar(d + '0');
 		n = n - p * d;
 		p = 1;
 	}
-	if (flag == 2)
-	{
+	if (flag == 1)
 		putchar('8');
-	}
-	return (0);
+	if (neg == 1)
+		return (len + neg + flag);
+	return (len);
 }
 
 /**
@@ -103,7 +107,7 @@ int print_numd(va_list selector)
  */
 int print_unsigned(va_list selector)
 {
-	int i, d, k = 1, p = 1, j, larg;
+	int i, d, k = 1, p = 1, j, larg, len;
 	unsigned int n;
 
 	n = (va_arg(selector, unsigned int));
@@ -114,19 +118,20 @@ int print_unsigned(va_list selector)
 		larg = larg / 10;
 		k++;
 	}
+	len = k;
 	for (i = k; i > 0; i--)
 	{
 		if (k != 1)
 		{
-		for (j = 2; j <= i; j++)
-		{
-			p = p * 10;
-		}
+			for (j = 2; j <= i; j++)
+			{
+				p = p * 10;
+			}
 		}
 		d = n / p;
 		putchar(d + '0');
 		n = n - p * d;
 		p = 1;
 	}
-	return (0);
+	return (len);
 }
