28901974c6bb702c0fcefb2cc2a8233c98bc6153
David Hincapie <1508@holbertonschool.com>
2020-03-17 16:10:43 +0000

    Add function for chars

---
+++ char.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_char - Funtion to print a char
+ *@selector: List of arguments valist
+ *
+ *
+ */
+void _char(va_list selector)
+{
+	char k = va_arg(selector, int);
+
+	_putchar(k);
+}
+
